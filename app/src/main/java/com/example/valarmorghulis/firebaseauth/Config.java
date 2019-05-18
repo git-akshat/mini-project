af038acfc0289789baaf1119fcb704d1e4a28acb
Akshat <mr.akshatagarwal26@gmail.com>
2019-05-18 10:53:21 +0530

    minor changes

--- app/src/main/java/com/example/valarmorghulis/firebaseauth/Config.java
+++ app/src/main/java/com/example/valarmorghulis/firebaseauth/Config.java
@@ -1,6 +1,6 @@
 package com.example.valarmorghulis.firebaseauth;
 
 public class Config {
-    public static final String EMAIL = "juggler.buy.sell@gmail.com";
+    public static final String EMAIL = "juggler.buy.and.sell@gmail.com";
     public static final String PASSWORD = "Miniproject";
 }
