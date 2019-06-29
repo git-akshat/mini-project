0e575e4479fce78622e6664396cbc970bc93e66c
Akshat <31710005+git-akshat@users.noreply.github.com>
2019-06-29 19:45:28 +0530

    Delete Config.java

--- app/src/main/java/com/example/valarmorghulis/firebaseauth/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.valarmorghulis.firebaseauth;
-
-public class Config {
-    public static final String EMAIL = "juggler.buy.and.sell@gmail.com";
-    public static final String PASSWORD = "Miniproject";
-}
